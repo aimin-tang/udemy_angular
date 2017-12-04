c94bc2e7bddfdb0a1891c5367c69685d09af2933
Aimin Tang <aimin.tang@gmail.com>
2017-12-04 11:54:15 -0600

    change recipe-list

--- recipes/recipe-list/recipe-list.component.ts
+++ recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Pad Thai', 'Tasty Thai street tood', 'https://c1.staticflickr.com/5/4050/4339908741_da16cb10c7_b.jpg'),
-    new Recipe('Pad Thai 2', 'Tasty Thai street tood', 'https://c1.staticflickr.com/5/4050/4339908741_da16cb10c7_b.jpg'),
+    new Recipe('Pad Thai 2', 'Tasty Thai street tood 2', 'https://c1.staticflickr.com/5/4050/4339908741_da16cb10c7_b.jpg'),
   ];
 
   constructor() { }
