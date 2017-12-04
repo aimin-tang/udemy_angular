7c089058ae5df3203baa18f20019ae91ec01901e
Aimin Tang <aimin.tang@gmail.com>
2017-12-03 22:00:33 -0600

    #47: recipe content

--- recipes/recipe-list/recipe-list.component.ts
+++ recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pad Thai',
+      'Tasty Thai street food',
+      'https://c1.staticflickr.com/5/4050/4339908741_da16cb10c7_b.jpg'),
+    new Recipe('Pad Thai',
+      'Tasty Thai street food',
+      'https://c1.staticflickr.com/5/4050/4339908741_da16cb10c7_b.jpg'),
+  ];
 
   constructor() { }
 
